320859ea03321bc32274bca9bcfb162a759a02df
ahmadou dia <dia.ahmadou23@gmail.com>
2022-11-22 17:15:21 +0100

    status seller a new page with all bookings of a user and all the links to navigate throug pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :devs do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :destroy]
   end
+  resources :bookings, only: :index
   # Defines the root path route ("/")
   # root "articles#index"
 end
