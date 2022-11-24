0faf1ec0bce279ce3d5d0cf8658173edb6118920
thibaut roland <thibaut.roland2@gmail.com>
2022-11-24 10:08:36 +0100

    map in the show

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -3,7 +3,7 @@ class DevsController < ApplicationController
 
   def index
     @devs = policy_scope(Dev)
-    @markers = @devs.geocoded.map do |dev|
+    @markers = @devs.map do |dev|
       {
         lat: dev.latitude,
         lng: dev.longitude
@@ -16,6 +16,11 @@ class DevsController < ApplicationController
   def show
     authorize @dev
     @booking = Booking.new
+
+    @marker = [{
+      lat: @dev.latitude,
+      lng: @dev.longitude
+    }]
   end
 
   def new
