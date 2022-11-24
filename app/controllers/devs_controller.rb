bbd21bd19afed2b101d2d6f3d4009752a6f13f2b
Arthur Foucaud <arthurfouc@gmail.com>
2022-11-24 13:06:44 +0100

    create new dev updated with address and price

--- app/controllers/devs_controller.rb
+++ app/controllers/devs_controller.rb
@@ -79,6 +79,6 @@ class DevsController < ApplicationController
   end
 
   def dev_params
-    params.require(:dev).permit(:name, :skill, :photo_url, :available, :photo)
+    params.require(:dev).permit(:name, :skill, :photo_url, :available, :photo, :address, :price)
   end
 end
