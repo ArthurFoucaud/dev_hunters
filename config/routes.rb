f5365240e351e9967bee868f2c1c6aef09689cfd
ahmadou dia <dia.ahmadou23@gmail.com>
2022-11-25 11:32:02 +0100

    dashboard with errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :devs do
     resources :bookings, only: [:new, :create, :destroy]
   end
   resources :bookings, only: :index
+  get "/dashboard", to: "pages#dashboard"
   # Defines the root path route ("/")
   # root "articles#index"
 end
